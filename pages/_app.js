a59b9d5794447f86e95f0da2e28679da4f6f3753
Terry Hunt <thunt4jr@gmail.com>
2022-10-17 07:47:53 -0400

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+import { StoreProvider } from "../utils/Store";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+      <StoreProvider>
+        <Component {...pageProps} />
+      </StoreProvider>
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
