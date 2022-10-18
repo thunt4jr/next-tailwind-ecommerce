f3d0112b966bce9a352acecee8049e8e7112112a
Terry Hunt <thunt4jr@gmail.com>
2022-10-18 07:03:22 -0400

    update

--- pages/payment.js
+++ pages/payment.js
@@ -8,13 +8,31 @@ export default function PaymentScreen() {
       <CheckoutWizard activeStep={2} />
       <form className='mx-auto max-w-screen-md' onSubmit={submitHandler}>
         <h1 className='mb-4 text-xl'>Payment Method</h1>
-        {
-            ['PayPal', 'Stripe', 'CashOnDelivery'].map((payment) => (
-                <div key={payment} className='mb-4'>
-                    <input name="paymentMethod" className="p-2 outline-none focus:ring-0" id={payment} type="radio" checked={selectedPaymentMethod === payment } onChange={() => setSelectedPaymentMethod(payment)} />
-                </div>
-            ))
-        }
+        {["PayPal", "Stripe", "CashOnDelivery"].map((payment) => (
+          <div key={payment} className='mb-4'>
+            <input
+              name='paymentMethod'
+              className='p-2 outline-none focus:ring-0'
+              id={payment}
+              type='radio'
+              checked={selectedPaymentMethod === payment}
+              onChange={() => setSelectedPaymentMethod(payment)}
+            />
+            <label className='p-2' htmlFor={payment}>
+              {payment}
+            </label>
+          </div>
+        ))}
+        <div className='mb-4 flex justify-between'>
+          <button
+            onClick={() => router.push("/shipping")}
+            type='button'
+            className='default-button'
+          >
+            Back
+          </button>
+          <button className='primary-button'>Next</button>
+        </div>
       </form>
     </Layout>
   );
